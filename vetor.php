45918f5d4f2984aac524b735a9aa63dbf41995bc
Francisco Johnatan Tavares Cavalcante <fcavalcante@localhost.localdomain>
2017-04-29 09:39:16 -0300

    Array e Range

--- vetor.php
+++ vetor.php
@@ -1,8 +1,18 @@
 <?php
+//Pode ser feito assim
+$n[0] = 2;
+$n[1] = 5;
+$n[2] = 1;
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Também pode ser feito assim
 
+$n = array(2,5,1);
+
+//Colocar o valor na ultima posição di meu vetor
+$n[] = 7;
+print_r($n);
+//Pode-se utilizar o RANGE também
+
+$c = range(5,20,5);
+//Meu primeiro valor vai ser 5, o ultimo vai ser 20 e ele vai pulando de 5 em 5.
+print_r($c);
